6d140564f8c9a167a6e1202e53c1e872acff09e3
asonnenschein <adrian.sonnenschein@gmail.com>
2015-02-11 21:57:05 -0700

    initial commit, start on harvester

---
+++ index.js
@@ -0,0 +1,6 @@
+/*********************************************
+ * csw-monitor                               *
+ * https://github.com/slippynode/csw-monitor *
+ * Copyright 2014 Adrian Sonnenschein        *
+ * Released under the MIT license            *
+ *********************************************/
